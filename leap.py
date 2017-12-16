f3d8bc004ccd43d290c7f348b2a439903da17038
divyasanchana <34593126+divyasanchana@users.noreply.github.com>
2017-12-16 14:45:49 +0530

    Add files via upload

--- leap.py
+++ leap.py
@@ -1,13 +1,24 @@
 x=int(raw_input())
 
-rem1=x%400
-
-rem2=x%4
-
-if(rem1==0 and rem2==0):
+if(x%4==0):
 	
-	print("Hurray! It's a leap year")
+	if(x%100==0):
+		
+		if(x%400==0):
+			
+			print("Hurray! It's a leap year")
+		
+		else:
+			
+			print("Nope. It's not a leap year")
+	
+	else:
+	   
+		print("Hurray! It's a leap year")	
+
 
 else:
 	
+	
 	print("Nope. It's not a leap year")
+
