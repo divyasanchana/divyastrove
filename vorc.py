c10f8c4f6caa5e9f5181b2e7a3c59a9a9233896f
divyasanchana <34593126+divyasanchana@users.noreply.github.com>
2017-12-16 11:56:42 +0530

    Add files via upload

---
+++ vorc.py
@@ -0,0 +1,27 @@
+x=raw_input()
+
+print x
+
+if(x=="a"):
+	
+	print("It's a vowel")
+
+elif (x=="e"): 
+	
+	print("It's a vowel")
+
+elif (x=="i"): 
+	
+	print("It's a vowel")
+
+elif (x=="o"): 
+	
+	print("It's a vowel")
+
+elif (x=="o"): 
+	
+	print("It's a vowel")
+
+else:
+	
+	print("It's a consonnant")
