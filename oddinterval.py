3bcf3138e2aef0304252f0330185a848365d97b7
divyasanchana <34593126+divyasanchana@users.noreply.github.com>
2018-06-12 09:11:06 +0530

    Update oddinterval.py

--- oddinterval.py
+++ oddinterval.py
@@ -2,8 +2,12 @@ x=input()
 y=input()
 x=int(x)
 y=int(y)
+c=[]
 for i in range(x,y,2):
 	if (i%2)==0:
-		print (i+1)
+		c.append (str(i+1))
 	else:
-		print (i)
+		c.append (str(i))
+print(c)
+c=' '.join(c)
+print(c)
