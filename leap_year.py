82194e9b0b9c40b624f7056d974186f925ff7388
divyasanchana <34593126+divyasanchana@users.noreply.github.com>
2018-06-22 12:08:17 +0530

    Rename leap.py to leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,24 @@
+x=int(raw_input())
+
+if(x%4==0):
+	
+	if(x%100==0):
+		
+		if(x%400==0):
+			
+			print("Hurray! It's a leap year")
+		
+		else:
+			
+			print("Nope. It's not a leap year")
+	
+	else:
+	   
+		print("Hurray! It's a leap year")	
+
+
+else:
+	
+	
+	print("Nope. It's not a leap year")
+
